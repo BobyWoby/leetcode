e70b2095893505836ccdb6ece4a1d0e1bc608b07
student <2025.kzheng@jburroughs.org>
2024-09-17 10:27:16 -0500

    fixed leetcode 70

--- leetcode/leetcode70.cpp
+++ leetcode/leetcode70.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <map>
 
 
 // Leetcode #70: Climbing stairs
@@ -8,21 +9,21 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
 
-int factorial(int n) {
-	if (n <= 0) return 1;
-	return n * factorial(n - 1);
-}
-int nCr(int n, int r) {
-	return factorial(n) / (factorial(r) * factorial(n - r));
-}
+std::map<int, int> memo = {};
+
 
 int climbStairs(int n) {
-	int twos = std::floor(n / 2);
-	int result = 0;
-	for (int i = 0; i <= twos; i++) {
-		result += nCr(n - i - i + 1, i);
-	}
-	return result;
+    if(memo.find(n) != memo.end()){
+        return memo[n];
+    }
+    if(n == 0){
+     return 1;
+    }
+    if(n < 0){
+        return 0;
+    }
+    memo[n] = climbStairs(n-1) + climbStairs(n-2);
+    return(memo[n]);
 }
 
 int main() {
