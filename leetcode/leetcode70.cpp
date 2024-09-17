b07d9123f38a1ef01e55e9fae89ccd773c9d21d1
BobyWoby <bestdogeever865@gmail.com>
2024-09-17 12:11:38 -0500

    added bottom up approach(faster) to leetcode 70

--- leetcode/leetcode70.cpp
+++ leetcode/leetcode70.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <map>
+#include <vector>
 
 
 // Leetcode #70: Climbing stairs
@@ -9,9 +10,19 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
 
+// Bottom Up
+int climbStairs1(int n) {
+    std::vector<int> vec(n+1, -1);
+    vec[0] = vec[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        vec[i] = vec[i - 1] + vec[i - 2];
+    }
+    return vec[n];
+}
+
+
+// Top Down
 std::map<int, int> memo = {};
-
-
 int climbStairs(int n) {
     if(memo.find(n) != memo.end()){
         return memo[n];
